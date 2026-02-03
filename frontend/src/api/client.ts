d1051b00afa36f5c941ec5e76225b6949b58d792
Shifa Tazeen <shifatazeen.msc@gmail.com>
2026-02-03 21:43:52 +0530

    Update client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: https://playto-ljc5.onrender.com/api/,
+  baseURL: "https://playto-ljc5.onrender.com/api/,
   headers: {
     "Content-Type": "application/json",
   },
